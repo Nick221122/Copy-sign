44bb3d8232a4faf9a84d25041e469b7d43f7495b
10kilos <malikjeremiassen86@gmail.com>
2026-09-17 03:10:34 -0100

    Fix sign screen mixin field shadowing

--- src/main/java/nick221122/copysign/mixin/SignEditScreenMixin.java
+++ src/main/java/nick221122/copysign/mixin/SignEditScreenMixin.java
@@ -3,14 +3,14 @@ package nick221122.copysign.mixin;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.block.entity.SignText;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import net.minecraft.util.Colors;
-import net.minecraft.client.gui.Selectable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,10 +19,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SignEditScreen.class)
 public abstract class SignEditScreenMixin extends Screen {
-    @Shadow protected abstract <T extends Element & Selectable & Drawable> T addDrawableChild(T drawable);
-    @Shadow protected int width;
-    @Shadow protected int height;
-    @Shadow protected final SignBlockEntity blockEntity = null;
+    @Shadow
+    protected abstract <T extends Element & Selectable & Drawable> T addDrawableChild(T drawable);
+
+    @Shadow
+    protected int width;
+
+    @Shadow
+    protected int height;
+
+    @Shadow
+    @Final
+    protected SignBlockEntity blockEntity;
 
     protected SignEditScreenMixin(Text title) {
         super(title);
